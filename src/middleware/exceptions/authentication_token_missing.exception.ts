c4172d549f1b696f9c0206faa4f93c79881bbce2
jakubmarchwiany <jakubmarchwiany@icloud.com>
2023-10-06 02:07:25 +0200

    refactor: update exceptions

---
+++ src/middleware/exceptions/authentication_token_missing.exception.ts
@@ -0,0 +1,7 @@
+import { HttpException } from "./http.exception";
+
+export class AuthenticationTokenMissingException extends HttpException {
+	constructor() {
+		super(401, "Brak tokenu autoryzacji");
+	}
+}
--- src/middleware/exceptions/wrong_authentication_token.ts
+++
@@ -1,8 +0,0 @@
-import HttpException from "./http";
-
-class WrongAuthenticationTokenException extends HttpException {
-    constructor() {
-        super(401, "Token autoryzacji jest nieprawidłowy");
-    }
-}
-export default WrongAuthenticationTokenException;
