a3da3f0032a38eca64746228b8a8905aff63931f
jakubmarchwiany <jakubmarchwiany@icloud.com>
2023-10-10 10:48:54 +0200

    feat: dateFrom must be in the past

--- src/middleware/schemas/habits/get_habits.schema.ts
+++ src/middleware/schemas/habits/get_habits.schema.ts
@@ -3,7 +3,9 @@ import { InferType, date, object, string } from "yup";
 const getHabitsSchema = object({
 	query: object({
 		myHabits: string().required("Query 'myHabits' wymagane"),
-		dateFrom: date().required("Query 'dateFrom' wymagane")
+		dateFrom: date()
+			.required("Query 'dateFrom' wymagane")
+			.max(new Date(), "'dateFrom' musi być przeszłością")
 	})
 });
 
